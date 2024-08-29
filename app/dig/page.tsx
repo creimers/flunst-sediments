648d5a415e996d05edab60cbb5b8f460ff48a6f1
Christoph Reimers <christoph@superservice-international.com>
2024-08-29 19:05:31 +0200

    this works

--- app/dig/page.tsx
+++ app/dig/page.tsx
@@ -5,8 +5,13 @@ export default function Dig() {
     fetch("/api/dig", { method: "POST" });
   }
   return (
-    <div>
-      <button onClick={doDig}>dig</button>
+    <div className="bg-white h-screen p-8 flex flex-col justify-end">
+      <button
+        onClick={doDig}
+        className="bg-yellow-300 text-black py-4 active:scale-95 font-semibold text-2xl"
+      >
+        BAGGERN
+      </button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,10 @@ const MUD_HEIGHT = 90;
 
 export default function Home() {
   const [clickCount, setClickCount] = React.useState(0);
+  const [mostRecentClick, setMostRecentClick] = React.useState<Date | null>(
+    null
+  );
+  const [clickAge, setClickAge] = React.useState(100000);
 
   useConnectionStateListener("connected", () => {
     console.log("Connected to Ably!");
@@ -25,15 +29,19 @@ export default function Home() {
     if (clickCount > 0) {
       setClickCount(clickCount - 2);
     }
+    if (mostRecentClick) {
+      setClickAge(new Date().getTime() - mostRecentClick!.getTime());
+    }
   }, 1000);
 
   function handleClick() {
     if (MUD_HEIGHT - clickCount > 10) {
       setClickCount(clickCount + 1);
     }
+    setMostRecentClick(new Date());
+    setClickAge(0);
   }
 
-  // const mudHeight = Math.max(90 - clickCount, 10);
   const mudHeight = MUD_HEIGHT - clickCount;
 
   return (
@@ -46,9 +54,16 @@ export default function Home() {
       <div className="h-screen w-12 bg-gradient-to-b from-white/50 via-transparent blur-xl absolute left-1/2 rotate-2"></div>
       <div className="h-screen w-4 bg-gradient-to-b from-white/70 via-white/20 blur-lg absolute left-1/4 -rotate-3"></div>
       <div
-        className="bg-gradient-to-b from-amber-700/70 to-amber-950 absolute bottom-0 left-0 z-10 w-screen"
-        style={{ height: `${mudHeight}vh` }}
+        className="bg-gradient-to-b from-amber-900/70 to-amber-950 absolute bottom-0 left-0 z-10 w-screen transition-transform h-screen"
+        style={{ transform: `translateY(${100 - mudHeight}vh)` }}
       ></div>
+      {mudHeight > 50 && clickAge > 5000 && (
+        <div className="absolute bottom-0 text-white z-50 w-screen py-8">
+          <p className="text-center">
+            Scanne den QR-Code und hilf, den Schlick auszubaggern!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
