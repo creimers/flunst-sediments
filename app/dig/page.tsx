e6e4791edda058ceddb9915029ed269473b2905e
Christoph Reimers <christoph@superservice-international.com>
2024-08-30 14:11:49 +0200

    cosmetics

--- app/dig/page.tsx
+++ app/dig/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
+import * as React from "react";
+
 export default function Dig() {
+  const [hasDug, setHasDug] = React.useState(false);
+
   function doDig() {
+    setHasDug(true);
     fetch("/api/dig", { method: "POST" });
   }
   return (
-    <div className="bg-white h-screen p-8 flex flex-col justify-end">
+    <div className="bg-white h-screen p-8 flex flex-col justify-end text-black">
+      {hasDug && (
+        <div className="my-auto text-center text-4xl font-black">
+          Gib alles!
+        </div>
+      )}
+      {!hasDug && (
+        <div className="animate-bounce space-y-4 py-4">
+          <div className="text-center">
+            Klicke hier, um den Schlick wegzubaggern.
+          </div>
+          <div className="text-center">&#8595;</div>
+        </div>
+      )}
       <button
         onClick={doDig}
         className="bg-yellow-300 text-black py-4 active:scale-95 font-semibold text-2xl"
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { useChannel, useConnectionStateListener } from "ably/react";
 
 import { useInterval } from "react-use";
 import { ABLY_CHANEL_NAME } from "@/lib/constants";
+import QRCode from "@/components/qr-code";
+import Wave from "@/components/wave";
 
 const MUD_HEIGHT = 90;
 
@@ -54,13 +56,19 @@ export default function Home() {
       <div className="h-screen w-12 bg-gradient-to-b from-white/50 via-transparent blur-xl absolute left-1/2 rotate-2"></div>
       <div className="h-screen w-4 bg-gradient-to-b from-white/70 via-white/20 blur-lg absolute left-1/4 -rotate-3"></div>
       <div
-        className="bg-gradient-to-b from-amber-900/70 to-amber-950 absolute bottom-0 left-0 z-10 w-screen transition-transform h-screen"
+        className=" absolute bottom-0 left-0 z-10 w-screen transition-transform h-screen flex flex-col"
         style={{ transform: `translateY(${100 - mudHeight}vh)` }}
-      ></div>
+      >
+        <div className="flex-shrink-0 text-stone-800 translate-y-2">
+          <Wave />
+        </div>
+        <div className="flex-1 bg-gradient-to-b from-stone-800 to-stone-950"></div>
+      </div>
       {mudHeight > 50 && clickAge > 5000 && (
-        <div className="absolute bottom-0 text-white z-50 w-screen py-8">
-          <p className="text-center">
-            Scanne den QR-Code und hilf, den Schlick auszubaggern!
+        <div className="absolute bottom-0 text-white z-50 w-screen py-8 flex flex-col items-center space-y-12">
+          <QRCode />
+          <p className="text-center text-3xl">
+            Scanne den QR-Code und hilf mit, den Schlick auszubaggern!
           </p>
         </div>
       )}
